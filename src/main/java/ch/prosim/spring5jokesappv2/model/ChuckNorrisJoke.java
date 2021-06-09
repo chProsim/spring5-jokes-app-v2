bb3f8f542dcb12438aff44de6414c4e6d5253d03
PatrickK <patrick.kehrli@prosim.ch>
2021-06-09 13:38:45 +0200

    Initial setup

---
+++ src/main/java/ch/prosim/spring5jokesappv2/model/ChuckNorrisJoke.java
@@ -0,0 +1,11 @@
+package ch.prosim.spring5jokesappv2.model;
+
+import ch.prosim.spring5jokesappv2.services.JokeService;
+
+public class ChuckNorrisJoke implements JokeService {
+
+    @Override
+    public String getJoke() {
+        return "";
+    }
+}
